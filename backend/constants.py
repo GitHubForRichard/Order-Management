7fe79b346a6de1c6b636ddf31d4c209759426f0d
Richard Liu <richard14942003@gmail.com>
2025-08-07 15:26:39 -0700

    Added docker configuration

--- backend/constants.py
+++ backend/constants.py
@@ -1,5 +1,7 @@
-DB_USER = "postgres"
-DB_PASSWORD = "supersecure"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "postgres"
+import os
+
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "supersecure")
+DB_HOST = os.getenv("POSTGRES_HOST", "postgres")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB", "postgres")
