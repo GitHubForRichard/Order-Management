08f5f10f76e69cddb2b78163d4967a121843dada
Richard Liu <richard14942003@gmail.com>
2025-08-13 08:38:20 -0700

    Updated customer list with material ui datagrid

--- frontend/src/CustomerList.jsx
+++ frontend/src/CustomerList.jsx
@@ -1,69 +1,52 @@
 import React from "react";
 
+import { DataGrid } from "@mui/x-data-grid";
+
 const CustomerList = ({ customers, onRowDoubleClicked }) => {
-  const [searchName, setSearchName] = React.useState("");
+  const columns = [
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter",
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.first_name || ""} ${row.last_name || ""}`,
+    },
+
+    {
+      field: "phoneNumber",
+      headerName: "Phone number",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 90,
+      valueGetter: (value, row) =>
+        `${row.phone_code || ""} ${row.phone_number || ""}`,
+    },
+    { field: "email", headerName: "Email", width: 160 },
+    {
+      field: "address",
+      headerName: "Address",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.street || ""}, ${row.city || ""}, ${row.state || ""}, ${
+          row.country || ""
+        } ${row.zip_code || ""}`,
+    },
+    { field: "loggedInUser", headerName: "Recorded By", width: 130 },
+    { field: "created_at", headerName: "Created Date", width: 130 },
+    { field: "updated_at", headerName: "Last Updated", width: 130 },
+  ];
 
   return (
-    <div className="form-left search-order">
-      {/* customer search bar */}
-      <div className="form-section-card search-bar">
-        <input
-          type="text"
-          className="form-input search-input"
-          placeholder="Search by customer name"
-          value={searchName}
-          onChange={(e) => setSearchName(e.target.value)}
-        />
-        <table>
-          <thead>
-            <tr>
-              <th>Customer Name</th>
-              <th>Phone Number</th>
-              <th>Email</th>
-              <th>Address</th>
-              <th>Recorded By</th>
-              <th>Created Date</th>
-              <th>Last Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers
-              .filter((customer) =>
-                [customer.first_name, customer.last_name]
-                  .join(" ")
-                  .toLowerCase()
-                  .includes(searchName.toLowerCase())
-              )
-              .map((customer, idx) => (
-                <tr
-                  key={idx}
-                  onDoubleClick={() =>
-                    onRowDoubleClicked && onRowDoubleClicked(customer)
-                  }
-                >
-                  <td>
-                    {customer.first_name} {customer.last_name}
-                  </td>
-                  <td>
-                    {customer.phone_code} {customer.phone_number}
-                  </td>
-                  <td>{customer.email}</td>
-                  <td>
-                    {customer.street}, {customer.city}, {customer.state},
-                    {customer.country} {customer.zip_code}
-                  </td>
-                  <td>{customer.loggedInUser}</td>
-                  <td>
-                    {new Date(customer.created_at).toLocaleDateString()}{" "}
-                    {new Date(customer.created_at).toLocaleTimeString()}
-                  </td>
-                  <td>{customer.last_updated}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <DataGrid
+      rows={customers}
+      columns={columns}
+      onRowDoubleClick={(params) =>
+        onRowDoubleClicked && onRowDoubleClicked(params.row)
+      }
+    />
   );
 };
 
