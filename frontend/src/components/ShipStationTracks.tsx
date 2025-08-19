cb32a87686b2622d8060b70c446c75f404a8384d
Richard Liu <richard14942003@gmail.com>
2025-08-19 13:22:41 -0700

    Still show ship station component even tracking number is not provided

--- frontend/src/components/ShipStationTracks.tsx
+++ frontend/src/components/ShipStationTracks.tsx
@@ -32,40 +32,38 @@ const ShipStationTracks = ({ trackingNumber }) => {
   }, [trackingNumber]);
 
   return (
-    trackingNumber && (
-      <div className="form-section-card shipstation-track">
-        <h3 className="section-title">ShipStation Track</h3>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Carrier</TableCell>
-                <TableCell>Service</TableCell>
-                <TableCell>Recipient</TableCell>
-                <TableCell>Ship Date</TableCell>
-                <TableCell>Tracking Number</TableCell>
+    <div className="form-section-card shipstation-track">
+      <h3 className="section-title">ShipStation Track</h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Carrier</TableCell>
+              <TableCell>Service</TableCell>
+              <TableCell>Recipient</TableCell>
+              <TableCell>Ship Date</TableCell>
+              <TableCell>Tracking Number</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {shipStationTracks.map((row) => (
+              <TableRow
+                key={row.carrierCode}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.carrierCode}</TableCell>
+                <TableCell>{row.serviceCode}</TableCell>
+                <TableCell>{row.shipTo?.name}</TableCell>
+                <TableCell>
+                  {new Date(row.shipDate).toLocaleDateString()}
+                </TableCell>
+                <TableCell>{row.trackingNumber}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {shipStationTracks.map((row) => (
-                <TableRow
-                  key={row.carrierCode}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>{row.carrierCode}</TableCell>
-                  <TableCell>{row.serviceCode}</TableCell>
-                  <TableCell>{row.shipTo?.name}</TableCell>
-                  <TableCell>
-                    {new Date(row.shipDate).toLocaleDateString()}
-                  </TableCell>
-                  <TableCell>{row.trackingNumber}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    )
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
